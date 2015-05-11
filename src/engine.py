b52344cff5690aaa9a73b75647dc52025d6baa40
parejadan <parejadan@gmail.com>
2015-05-11 14:54:42 -0400

    updated mane for production use

--- src/engine.py
+++ src/engine.py
@@ -6,7 +6,6 @@ from sys import argv;
 import os, urllib, json, time;
 
 os_brack = '/'; #directory separator for os engine is running on
-categories = [1250, 4500, 8000, 16000, 35000];
 
 def loadData(datSRC, path, delim, typ):
 	return loadtxt(path + os_brack + datSRC, delimiter=delim, dtype = typ);
@@ -37,15 +36,16 @@ def makeClassifier(data):
 	'trains a classifier with a given training data'
 	nv = NaiveBayes();
 	nv.summarizeByClass(data); #train classififer
-	#f = open( 'classifier.pickle', 'wb');
-	#dump(nv, f); #save trained classififer as python pickle
-	#f.close();
+	f = open( 'classifier.pickle', 'wb');
+	dump(nv, f); #save trained classififer as python pickle
+	f.close();
 	return nv; #return trained classififer
 
-def discretizeFreq(frequency, cats = categories):
+def discretizeFreq(frequency, cats = [1250, 4500, 8000, 16000, 35000]):
 	'categorizes result hits from a google saerch query \
 	if no categories are passed, it uses the default defined'
-	for i in len(cats):
+	#print cats;
+	for i in range( len(cats) ):
 		if frequency < cats[i]:
 			return i+1;
 	return len(cats)+1;
@@ -87,42 +87,70 @@ def testClassifier(examples, trnprt, tstprt, size):
 
 	return (1 - falses/(size-trnprt)), (avgLvl/len(tstset));
 
+def getWordCountDif(txt1, txt2, delim =' '):
+	return abs( len(txt1.split(delim)) - len(txt2.split(delim)) );
 
 def main():
 
-	#classifier = loadClassifier();
-	#if classifier is None:
-	datOrder = 'random'
-	accurDat = '';
-	confiDat = '';
-	for j in arange(0.5, 1, 0.1): #threashold increases
-		for k in arange(2.0,6): #training data decreases
-			accurDat += '%f %f ' % ((1/k), j);
-			confiDat += '%f %f ' % ((1/k), j);
+	#setup classifier
+	thresh = 0.65;
+	classifier = loadClassifier();
+	if classifier is None:
+		path = '..' + os_brack + 'training-data' + os_brack + '3-dimensional';
+		examples = loadData('data-random.csv' , path, ',', float);
+		examples = discretizeTarget(examples, thresh);
+		trnprt = len(examples)/3;
+		trnset = examples[:trnprt];
+		classifier = makeClassifier(trnset);
 
-			for i in range(1,4): #dimensions increase
-				path = '..' + os_brack +'training-data' + os_brack + '%d-dimensional' % (i+1);
-				#print path
-				examples = loadData('data-%s.csv' % datOrder, path, ',', float);
-				examples = discretizeTarget(examples, j);
-				size = len(examples);
+	#junk -> name of this python file, not needed but automatically passed by interpreter 
+	#srcTxt -> source text user translated
+	#dstTxt -> translated text
+	#srcLng -> language of source txt
+	#dstTtxt -> language source text was translated to
+	#srcTxt, dstTxt, srcLng, dstLng = loadData(argv[1], '../input', '\n', str); #use this interface for basic testing
+	junk, srcTxt, dstTxt, srcLng, dstLng = argv; #use this interface for production
 
-				trnprt = size/k;
-				tstprt = trnprt;
-				accuracy, confidence = testClassifier(examples, trnprt, tstprt, size);
+	#setup input data
+	frequency = discretizeFreq( googleSearch(dstTxt) );
+	wordDif = getWordCountDif(srcTxt, dstTxt);
+	txtlen = len(srcTxt);
 
-				accurDat += '%f ' % (accuracy);
-				confiDat += '%f ' % (confidence);
-			accurDat += '\n';
-			confiDat += '\n';
+	#make prediction
+	label, prob = classifier.predict( [txtlen, frequency, wordDif] );
+	prediction = ''; prob *= 100; #convert prob to a percentage
+	if label == 1: prediction = 'good';
+	else: prediction = 'bad';
 
+	#display prediction
+	print '\nPredicted translation type: %s' % prediction;
+	print 'Prediction confidence percentage: %f' % prob;
+	print 'Classifier\'s word-to-word equivalence threshold percentage %f\n' % (thresh * 100);
 
-	saveData(accurDat, ('acur-%s.dat' % datOrder), 'w');
-	saveData(confiDat, ('conf-%s.dat' % datOrder), 'w');
-	print 'data organization is %s\n' % datOrder;
-
-
-
-#if-condition executes main functions when file used directly
 if __name__ == '__main__':
 	main();
+
+
+#################Code reserved for classifier intensive testing################## 
+#	datOrder = 'random'
+#	accurDat = '';
+#	confiDat = '';
+#	for j in arange(0.5, 1, 0.1): #threashold increases
+#		for k in arange(2.0,6): #training data decreases
+#			accurDat += '%f %f ' % ((1/k), j);
+#			confiDat += '%f %f ' % ((1/k), j);
+#			for i in range(1,4): #dimensions increase
+#				path = '..' + os_brack +'training-data' + os_brack + '%d-dimensional' % (i+1);
+#				examples = loadData('data-%s.csv' % datOrder, path, ',', float);
+#				examples = discretizeTarget(examples, j);
+#				size = len(examples);
+#				trnprt = size/k;
+#				tstprt = trnprt;
+#				accuracy, confidence = testClassifier(examples, trnprt, tstprt, size);
+#				accurDat += '%f ' % (accuracy);
+#				confiDat += '%f ' % (confidence);
+#			accurDat += '\n';
+#			confiDat += '\n';
+#	saveData(accurDat, ('acur-%s.dat' % datOrder), 'w');
+#	saveData(confiDat, ('conf-%s.dat' % datOrder), 'w');
+#	print 'data organization is %s\n' % datOrder;
